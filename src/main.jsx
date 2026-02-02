e61cfca4ff8d6cb98768940275bd4cc2fd351cb7
toni <canillask8@gmail.com>
2026-02-02 14:28:53 +0100

    fix: add HashRouter import and wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom";
-
+import  {HashRouter } from "react-router-dom";
 
 import { store } from "./app/store.js"
 import "./index.css"
